d18de64a61bb89924c53a0fb82b625381c255a0d
001ryu-ryu <iftikarraza8@gmail.com>
2024-07-05 13:54:42 +0530

    learned primitive and other stuffs

---
+++ Primitives.java
@@ -0,0 +1,12 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollNo = 22003;
+        char ch = 'm';
+        float cgpa = 6.5f;
+        double largeDecimalNumbers = 74589475.74;
+        long largeInteger = 12378934790234829L;
+        boolean check = true;
+
+        // String is not primitive
+    }
+}
