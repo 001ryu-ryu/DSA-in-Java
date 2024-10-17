af6eb67410aeba02daba4aa9b6f286a7962c30e5
Iftikar Hussain <iftikarraza8@gmail.com>
2024-10-17 07:31:09 +0530

    Done linked list

---
+++ 13. LinkedList/linkedlist2/jcf/Classroom.java
@@ -0,0 +1,16 @@
+package linkedlist2.jcf;
+import java.util.LinkedList;
+public class Classroom {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addFirst(0);
+        System.out.println(ll);
+
+        ll.remove(1);
+        System.out.println(ll);
+    }
+
+}
